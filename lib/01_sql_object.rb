af49857ae3ffacc0d116792fc5e82a0468d0a9b8
Eric Savadian <eric.j.savadian@gmail.com>
2015-04-10 14:06:21 -0400

    Init Git, Create ::columns, ::finalize, ::table_name(=) methods

---
+++ lib/01_sql_object.rb
@@ -0,0 +1,66 @@
+require_relative 'db_connection'
+require 'active_support/inflector'
+# NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
+# of this project. It was only a warm up.
+
+class SQLObject
+  def self.columns
+    table = DBConnection.execute2("SELECT * FROM #{table_name}")
+    columns = []
+    table[0].each do |col|
+      columns << col.to_sym
+    end
+    columns
+  end
+
+  def self.finalize!
+    columns.each do |col|
+      define_method("#{col}=") { |args| attributes[col] = args }
+      define_method(col) { attributes[col] }
+    end
+  end
+
+  def self.table_name=(table_name)
+    @table_name = table_name
+  end
+
+  def self.table_name
+    @table_name ||= self.name.tableize
+  end
+
+  def self.all
+    # ...
+  end
+
+  def self.parse_all(results)
+    # ...
+  end
+
+  def self.find(id)
+    # ...
+  end
+
+  def initialize(params = {})
+    # ...
+  end
+
+  def attributes
+    @attributes ||= {}
+  end
+
+  def attribute_values
+    # ...
+  end
+
+  def insert
+    # ...
+  end
+
+  def update
+    # ...
+  end
+
+  def save
+    # ...
+  end
+end
